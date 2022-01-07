146a487e5e206b486db8cad4f1bb72cf1df7c4e6
Aji Furqon <furqonajiy@caditglobal.com>
2022-01-08 01:34:12 +0700

    Update library

--- inventory_management_system/src/main/java/io/furqonajiy/inventory_management_system/controller/InventoryController.java
+++ inventory_management_system/src/main/java/io/furqonajiy/inventory_management_system/controller/InventoryController.java
@@ -1,12 +1,5 @@
 package io.furqonajiy.inventory_management_system.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import io.furqonajiy.inventory_management_system.repository.InventoryRepository;
-
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
--- inventory_management_system/src/main/java/io/furqonajiy/inventory_management_system/exception/ResourceNotFoundException.java
+++ inventory_management_system/src/main/java/io/furqonajiy/inventory_management_system/exception/ResourceNotFoundException.java
@@ -3,7 +3,6 @@ package io.furqonajiy.inventory_management_system.exception;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(value = HttpStatus.NOT_FOUND)
 public class ResourceNotFoundException extends RuntimeException {
